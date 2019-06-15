514b2b4d541dbcee923defba043aba9fd22d1c4f
Pranshu Maheshwari <maheshwari.pranshu511@gmail.com>
2019-06-15 17:12:28 +0530

    Added persisted state

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as Cookies from 'js-cookie'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  plugins: [
+    createPersistedState({
+      getState: (key) => Cookies.getJSON(key),
+      setState: (key, state) => Cookies.set(key, state, { expires: 3, secure: true })
+    })
+  ]
 }).$mount('#app')
